674e54fc756ea6c013a9c80bf2f364fe156f3ddf
zdd <zddhub@gmail.com>
2022-05-18 21:33:25 +0800

    Initial commit

---
+++ RestaurantOrder/RestaurantOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantOrderApp.swift
+//  RestaurantOrder
+//
+//  Created by Dongdong Zhang on 2022/5/18.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
