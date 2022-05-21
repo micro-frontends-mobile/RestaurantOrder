065b8f91a50cb5b7a3fc9c7f2e4b08726ae4f89a
zdd <zddhub@gmail.com>
2022-05-21 16:35:57 +0800

    Add Example App

---
+++ RestaurantOrderExample/RestaurantOrderExample/RestaurantOrderExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantOrderExampleApp.swift
+//  RestaurantOrderExample
+//
+//  Created by Dongdong Zhang on 2022/5/21.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantOrderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
